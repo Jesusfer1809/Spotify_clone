144d8857e52be3e08ece9d1bd5277c74fe9f62b2
Jesus Rondon <jesusf.rondon1809@gmail.com>
2022-01-13 23:36:26 -0500

    xd

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,7 +3,13 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   // TOKEN WILL EXIST IF USER IS LOGGED In
-  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.JWT_SECRET,
+    secureCookie:
+      process.env.NEXTAUTH_URL?.startsWith("https://") ??
+      !!process.env.VERCEL_URL,
+  });
 
   const { pathname } = req.nextUrl;
 
