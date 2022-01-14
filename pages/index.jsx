aac457b4288810f22cd564191d0bb7fcc45bf8df
Jesus Rondon <jesusf.rondon1809@gmail.com>
2022-01-13 23:47:55 -0500

    ssA

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import Player from "../components/Player";
 import { getSession } from "next-auth/react";
 
 export default function Home() {
-  //xs
+  //xd
   return (
     <div className=" font-rubik h-screen bg-black overflow-hidden">
       <Head>
